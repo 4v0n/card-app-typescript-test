31b344aacbb0ac9c43a1cd8eaa1a38569e1e1556
4v0n <navodhk@hotmail.com>
2024-08-28 19:43:24 +0000

    feat: add functionality to dark mode button and persist mode in localStorage

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,11 +1,45 @@
-import {NavLink} from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export default function NavBar(){
-    return(
-      <nav className="flex justify-center gap-5">
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/create'}>New Entry</NavLink>
-        <button className="m-3 p-4 text-xl bg-gray-300 hover:bg-gray-400 rounded-md font-medium text-black">Toggle Dark Mode</button>
-      </nav>
-    )
+export default function NavBar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("dark-mode");
+    if (savedTheme === "enabled") {
+      setDarkMode(true);
+      document.documentElement.classList.add("dark");
+    }
+  }, [])
+
+  const toggleDarkMode = () => {
+    if (darkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("dark-mode", "disabled");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("dark-mode", "enabled");
+    }
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <nav className="flex justify-center gap-5">
+      <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={"/"}>
+        All Entries
+      </NavLink>
+      <NavLink
+        className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
+        to={"/create"}
+      >
+        New Entry
+      </NavLink>
+      <button
+        onClick={toggleDarkMode}
+        className="m-3 p-4 text-xl bg-gray-300 hover:bg-gray-400 rounded-md font-medium text-black"
+      >
+        {darkMode ? "light Mode" : "Dark Mode"}
+      </button>
+    </nav>
+  );
 }
