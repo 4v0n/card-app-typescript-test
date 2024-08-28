92ea75e98061913132d5645fc8863fbd37ad87a3
4v0n <navodhk@hotmail.com>
2024-08-28 19:03:10 +0000

    feat: add dark mode toggle button to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -5,6 +5,7 @@ export default function NavBar(){
       <nav className="flex justify-center gap-5">
         <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/'}>All Entries</NavLink>
         <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/create'}>New Entry</NavLink>
+        <button className="m-3 p-4 text-xl bg-gray-300 hover:bg-gray-400 rounded-md font-medium text-black">Toggle Dark Mode</button>
       </nav>
     )
 }
